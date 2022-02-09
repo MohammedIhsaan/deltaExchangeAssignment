fdb517af4106a25b56be424218ea89b1e043c1c8
MohammedIhsaan <mohammedihsaanulhaque@gmail.com>
2022-02-10 01:16:14 +0530

    working on filter button

--- src/components/TeamMember.js
+++ src/components/TeamMember.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import "./teamMember.css";
 
@@ -7,6 +7,11 @@ import ActionTypes from "../redux/constants/actionTypes";
 
 export default function TeamMember() {
   let data = useSelector((state) => state.allMember.member);
+  const [classList, setclassList] = useState("visible");
+  const [isChecked, setIsChecked] = useState(
+    new Array(data.length).fill(false)
+  );
+  const [isCheckedAll, setIsCheckedAll] = useState(false);
   //   const [data, setdata] = useState(data);
   const dispatch = useDispatch();
 
@@ -26,6 +31,35 @@ export default function TeamMember() {
       })
     );
   };
+  let addClass = "dropdown-check-list " + classList;
+  const handleSelect = () => {
+    console.log("select");
+    classList === "visible" ? setclassList("") : setclassList("visible");
+  };
+
+  const handleChangeAll = () => {
+    console.log("changed");
+
+    setIsCheckedAll(!isCheckedAll);
+  };
+  const handleChange = (e) => {
+    // console.log(e.target, isChecked);
+
+    const updatedisChecked = isChecked.map((item, index) =>
+      index === Number(e.target.id) ? !item : item
+    );
+    setIsChecked(updatedisChecked);
+  };
+
+  console.log(
+    "checking",
+    isChecked.every((e) => e === true)
+  );
+
+  useEffect(() => {
+    console.log("from useEffect", isChecked);
+    isChecked.every((e) => e === true) && setIsCheckedAll(!isCheckedAll);
+  }, [isChecked]);
 
   return (
     <div className="table">
@@ -36,13 +70,39 @@ export default function TeamMember() {
         </button>
       </div>
       <hr />
-      <select name="subject" id="subject">
-        <option value="">Select subject</option>
-      </select>
+      <div id="list1" className={addClass} tabIndex="100">
+        <span onClick={handleSelect} className="anchor">
+          Company({2})
+        </span>
+        <ul className="items">
+          <li>
+            <input
+              checked={isCheckedAll}
+              onChange={handleChangeAll}
+              type="checkbox"
+            />
+            Select All
+          </li>
+          {data?.map((obj, index) => {
+            return (
+              <li>
+                <input
+                  onChange={handleChange}
+                  type="checkbox"
+                  id={index}
+                  checked={isChecked[index]}
+                />
+                {obj.company}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
 
       <table>
         <tr>
           <th></th>
+          <th>Name</th>
           <th>Company</th>
           <th>Status</th>
           <th>Last Updated</th>
@@ -51,7 +111,10 @@ export default function TeamMember() {
         {data?.map((obj) => {
           return (
             <tr id={obj.id} key={obj.id}>
-              <th>o</th>
+              <th>
+                <input type="checkbox" id="vehicle1" />
+              </th>
+              <td>{obj.name}</td>
               <td>{obj.company}</td>
               <td>{obj.status}</td>
               <td>{obj.lastUpdate}</td>

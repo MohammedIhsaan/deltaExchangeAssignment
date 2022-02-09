66913e4c7b29401a23d9090a36e0be9750a2dc7b
MohammedIhsaan <mohammedihsaanulhaque@gmail.com>
2022-02-09 20:43:58 +0530

    addMmber and deleteMember functionality is added

--- src/components/TeamMember.js
+++ src/components/TeamMember.js
@@ -1,37 +1,43 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import "./teamMember.css";
 
+import { addMemeber, deleteMemeber } from "../redux/actions/action";
+import ActionTypes from "../redux/constants/actionTypes";
+
 export default function TeamMember() {
-  let data = [
-    {
-      company: "wipro",
-      status: "Active",
-      lastUpdate: "01/02/2022",
-    },
-    {
-      company: "infosy",
-      status: "Close",
-      lastUpdate: "01/02/2022",
-    },
-    {
-      company: "tcs",
-      status: "Active",
-      lastUpdate: "01/02/2022",
-    },
-  ];
+  let data = useSelector((state) => state.allMember.member);
+  //   const [data, setdata] = useState(data);
+  const dispatch = useDispatch();
+
+  const handelClick = (e) => {
+    let elementId = e.target.id;
+    dispatch(deleteMemeber(elementId));
+  };
+
+  const handleSubmit = () => {
+    console.log("submit");
+    dispatch(
+      addMemeber({
+        id: 3,
+        company: "myCompany",
+        status: "Act",
+        lastUpdate: "01/02/2022",
+      })
+    );
+  };
+
   return (
     <div className="table">
       <div className="teamMember">
         <h2>Team Members</h2>
-        <button className="addBuuton">
+        <button onClick={handleSubmit} className="addBuuton">
           Add Members <span>+</span>
         </button>
       </div>
       <hr />
       <select name="subject" id="subject">
-        <option value="" selected="selected">
-          Select subject
-        </option>
+        <option value="">Select subject</option>
       </select>
 
       <table>
@@ -42,9 +48,9 @@ export default function TeamMember() {
           <th>Last Updated</th>
           <th>Notes</th>
         </tr>
-        {data.map((obj) => {
+        {data?.map((obj) => {
           return (
-            <tr>
+            <tr id={obj.id} key={obj.id}>
               <th>o</th>
               <td>{obj.company}</td>
               <td>{obj.status}</td>
@@ -53,6 +59,8 @@ export default function TeamMember() {
               <td>
                 <span>
                   <img
+                    id={obj.id}
+                    onClick={handelClick}
                     className="delete_icon"
                     src="https://img.icons8.com/material/48/000000/filled-trash.png"
                   />
--- src/redux/reducers/reducer.js
+++ src/redux/reducers/reducer.js
@@ -3,16 +3,19 @@ import actionTypes from "../constants/actionTypes";
 const initailState = {
   member: [
     {
+      id: 0,
       company: "wipro",
       status: "Active",
       lastUpdate: "01/02/2022",
     },
     {
+      id: 1,
       company: "infosy",
       status: "Close",
       lastUpdate: "01/02/2022",
     },
     {
+      id: 2,
       company: "tcs",
       status: "Active",
       lastUpdate: "01/02/2022",
@@ -20,12 +23,14 @@ const initailState = {
   ],
 };
 
-export const reducer = (state = initailState, { type, paylaod }) => {
+export const reducer = (state = initailState, { type, payload }) => {
   switch (type) {
     case actionTypes.ADD_MEMBER:
-      return state;
+      let updateMember = [...state.member, payload];
+      return { ...state, member: updateMember };
     case actionTypes.DELETE_MEMBER:
-      return state;
+      let filterMember = state.member.filter((e) => e.id !== Number(payload));
+      return { ...state, member: filterMember };
     default:
       return state;
   }
